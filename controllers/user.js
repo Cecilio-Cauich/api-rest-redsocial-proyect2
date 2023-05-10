9c19d3ddb28d456283551d2361ea77771df3ea7d
Cecilio-Cauich <celiocatu@gmail.com>
2023-05-09 18:03:59 -0600

    Funcion de actualizar usuario

--- controllers/user.js
+++ controllers/user.js
@@ -2,7 +2,7 @@
 import user from "../models/user.js";
 import bcrypt from "bcrypt";
 import jwt from "../services/jwt.js";
-import mongoosePaginate from "mongoose-pagination";
+//import mongoosePaginate from "mongoose-pagination";
 
 //acciones de prueba
 const testUser = (req, res) => {
@@ -24,47 +24,54 @@ const register = (req, res) => {
       message: "Missing data to send",
     });
   }
-
-  //control de usuarios duplicados
-  user
-    .find({
-      $or: [
-        { email: params.email.toLowerCase() },
-        { nick: params.nick.toLowerCase() },
-      ],
-    })
-    .exec()
-    .then(async (users) => {
-      if (users && users.length >= 1) {
-        return res.status(200).send({
-          status: "Succes",
-          message: "User already exists",
-        });
-      }
-
-      //cifrar la contraseña
-      let pwd = await bcrypt.hash(params.password, 10);
-      params.password = pwd;
-
-      //crear objeto de usuario para guardar el usuario
-      let user_to_save = new user(params);
-
-      //Guardar usuario en la base de datos
-      user_to_save.save().then((user_stored) => {
-        if (!user_stored) {
-          return res.status(400).json({
-            status: "error",
-            message: "Error trying to save user",
+  try {
+    //control de usuarios duplicados
+    user
+      .find({
+        $or: [
+          { email: params.email.toLowerCase() },
+          { nick: params.nick.toLowerCase() },
+        ],
+      })
+      .exec()
+      .then(async (users) => {
+        if (users && users.length >= 1) {
+          return res.status(200).send({
+            status: "Succes",
+            message: "User already exists",
           });
         }
-        //devolver resultado
-        return res.status(200).json({
-          status: "Succes",
-          message: "User save successfully",
-          params,
+
+        //cifrar la contraseña
+        let pwd = await bcrypt.hash(params.password, 10);
+        params.password = pwd;
+
+        //crear objeto de usuario para guardar el usuario
+        let user_to_save = new user(params);
+
+        //Guardar usuario en la base de datos
+        user_to_save.save().then((user_stored) => {
+          if (!user_stored) {
+            return res.status(400).json({
+              status: "error",
+              message: "Error trying to save user",
+            });
+          }
+          //devolver resultado
+          return res.status(200).json({
+            status: "Succes",
+            message: "User save successfully",
+            params,
+          });
         });
       });
+  } catch (error) {
+    return res.status(500).json({
+      status: "error",
+      mensaje: "Register failer",
+      error: error.message,
     });
+  }
 };
 
 //************************************** LOGIN *******************************
@@ -80,79 +87,98 @@ const login = (req, res) => {
   }
 
   //Buscar en la bdd si existe ese usuario
-  user
-    .findOne({ email: params.email })
-    .select({
-      //"password":0
-    })
-    .exec()
-    .then((user_f) => {
-      if (!user_f) {
-        return res.status(404).send({
-          status: "error",
-          message: "User does not exist",
+  try {
+    user
+      .findOne({ email: params.email })
+      .select({
+        //"password":0
+      })
+      .exec()
+      .then((user_f) => {
+        if (!user_f) {
+          return res.status(404).send({
+            status: "error",
+            message: "User does not exist",
+          });
+        }
+
+        //Comprobar su contraseña
+        let pwd = bcrypt.compareSync(params.password, user_f.password);
+        if (!pwd) {
+          return res.status(400).send({
+            status: "Error",
+            message: "The credentials are not correct",
+          });
+        }
+
+        //Devolver Token
+        const token = jwt.createToken(user_f);
+
+        //Devolver Datos del usuario
+        return res.status(200).send({
+          status: "succes",
+          message: " You have correctly identified",
+          user: {
+            id: user_f.id,
+            name: user_f.name,
+            nick: user_f.nick,
+          },
+          token,
         });
-      }
-
-      //Comprobar su contraseña
-      let pwd = bcrypt.compareSync(params.password, user_f.password);
-      if (!pwd) {
-        return res.status(400).send({
-          status: "Error",
-          message: "The credentials are not correct",
-        });
-      }
-
-      //Devolver Token
-      const token = jwt.createToken(user_f);
-
-      //Devolver Datos del usuario
-      return res.status(200).send({
-        status: "succes",
-        message: " You have correctly identified",
-        user: {
-          id: user_f.id,
-          name: user_f.name,
-          nick: user_f.nick,
-        },
-        token,
       });
+  } catch (error) {
+    return res.status(500).json({
+      status: "error",
+      mensaje: "Logging failed",
+      error: error.message,
     });
+  }
 };
 
 //*********************************OBTENER DATOS DEL PERFIL DEL USUARIO *****************
 const profile = (req, res) => {
   //Recibir el paramatro del id de usuario por la URL
+  let usuario = req.body;
+  console.log(usuario);
   const id = req.params.id;
 
-  //Consultar para sacar datos del usuario
-  user
-    .findById(id)
-    .select({
-      password: 0,
-      role: 0,
-    })
-    .exec()
-    .then((user_profile) => {
-      if (!user_profile) {
-        return res.status(404).send({
-          status: "Error",
-          message: "User does not exist or there is an error",
+  try {
+    //Consultar para sacar datos del usuario
+    user
+      .findById(id)
+      .select({
+        password: 0,
+        role: 0,
+      })
+      .exec()
+      .then((user_profile) => {
+        if (!user_profile) {
+          return res.status(404).send({
+            status: "Error",
+            message: "User does not exist or there is an error",
+          });
+        }
+
+        return res.status(200).send({
+          status: "Success",
+          user: user_profile,
         });
-      }
-
-      return res.status(200).send({
-        status: "Success",
-        user: user_profile,
       });
-    });
 
-  //Devolver resultado
+    //Devolver resultado
+  } catch (error) {
+    return res.status(500).json({
+      status: "error",
+      mensaje: "Getting user information failed",
+      error: error.message,
+    });
+  }
 };
 
 //*********************************OBTENER LISTA DE USUARIO********************
 const list = (req, res) => {
   //controlar en que paginas estamos
+
   let page = 1; //por default la pagina es 1
   if (req.params.page) {
     page = req.params.page; //si nos lo mandan como param entonces tomamos ese valor
@@ -161,32 +187,113 @@ const list = (req, res) => {
 
   //Consulta con mongoose pagination
   let itemsPerPage = 5;
+  try {
+    user
+      .find()
+      .sort("_id")
+      .paginate(page, itemsPerPage)
+      .then(async (users) => {
+        const total = await user.countDocuments({}).exec(); //obtener total
 
-  user
-    .find()
-    .sort("_id")
-    .paginate(page, itemsPerPage)
-    .then(async (users) => {
+        if (!users) {
+          return res.status(404).send({
+            status: "Error",
+            message: "There is not users available ",
+          });
+        }
+        //Devolver resultado
+        return res.status(200).send({
+          status: "Success",
+          users,
+          page,
+          itemsPerPage,
+          total: total,
+          pages: Math.ceil(total / itemsPerPage),
+        });
+      });
+  } catch (error) {
+    return res.status(500).json({
+      status: "error",
+      mensaje: "Listing failed",
+      error: error.message,
+    });
+  }
+};
+//*********************************ACTUALIZAR ASUARIO********************
+const update = (req, res) => {
+  //Recoger info del usuario a actualizar
+  let userIdentity = req.user;
+  let userToUpdate = req.body;
 
-      const total = await user.countDocuments({}).exec();//obtener total
+  //Eliminar campos sombrantes
+  delete userToUpdate.iat;
+  delete userToUpdate.exp;
+  delete userToUpdate.role;
+  delete userToUpdate.image;
 
-      if(!users){
-        return res.status(404).send({
-          status: "Error",
-          message: "There is not users available ",
+  //Comprobar si el usuario ya existe
+  try {
+    user
+      .find({
+        $or: [
+          { email: userToUpdate.email.toLowerCase() },
+          { nick: userToUpdate.nick.toLowerCase() },
+        ],
+      })
+      .exec()
+      .then(async (users) => {
+
+        let userIsset = false;
+
+        //Buscamos dentro de los usuarios que devuelve find el que sea igual al logeado
+        users.forEach((user_f) => {
+          if (user_f && user_f._id != userIdentity.id) {
+            userIsset = true;
+          }
         });
 
-      }
-      //Devolver resultado
-      return res.status(200).send({
-        status: "Success",
-        users,
-        page,
-        itemsPerPage,
-        total: total,
-        pages: Math.ceil(total/itemsPerPage)
+        if (userIsset) {
+          return res.status(200).send({
+            status: "Succes",
+            message: "User already exists",
+          });
+        }
+
+        //cifrar la contraseña
+        if (userToUpdate.password) {
+          let pwd = await bcrypt.hash(userToUpdate.password, 10);
+          userToUpdate.password = pwd;
+        }
+        //Buscar y actualizar
+        user.findByIdAndUpdate(userIdentity.id, userToUpdate,{new: true})
+        .then(userUpdated=>{
+          if(!userUpdated){
+            return res.status(500).send({
+              status: "Error",
+              message: "Update Failed"
+            });
+          }
+          
+          //devolver respuesta
+          return res.status(200).send({
+            status: "Succes",
+            message: "User updated successfully",
+            user: userUpdated
+          });
+
+        });
+
+
       });
+
+
+  } catch (error) {
+    return res.status(500).json({
+      status: "error",
+      mensaje: "Actualization failed",
+      error: error.message,
     });
+  }
 };
 export default {
   testUser,
@@ -194,4 +301,5 @@ export default {
   login,
   profile,
   list,
+  update,
 };
--- routers/user.js
+++ routers/user.js
@@ -9,7 +9,7 @@ router.post("/register", userController.register);
 router.post("/login", userController.login);
 router.get("/profile/:id", check.auth,userController.profile);
 router.get("/list/:page?", check.auth, userController.list);
-router.put("/update",check.auth, userController.update);
+router.put("/update", check.auth,userController.update);
 
 
 //Exportar el router
